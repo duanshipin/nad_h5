7923094f4ef9a5db9d1e8cc01799fdf5baba27cb
wangliaoyuan <wangliaoyuan@55haitao.com>
2023-08-17 12:34:31 +0800

    proxy server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const {
+    createProxyMiddleware
+} = require('http-proxy-middleware');
+
+const app = express();
+
+// 创建反向代理
+const proxy = createProxyMiddleware('/api/', {
+    target: 'https://mianshuixing.com/api/',
+    changeOrigin: true,
+    secure: false,
+});
+
+// 将代理附加到 Express 应用
+app.use('/api/', proxy);
+
+// 静态文件服务器
+app.use(express.static(__dirname + '/'));
+
+// 启动服务器
+const port = 8080;
+app.listen(port, () => {
+    console.log(`服务器已启动在 http://localhost:${port}`);
+});
